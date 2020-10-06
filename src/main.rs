d8e448e741c274f168e7ed5c6618802ad4f59985
Zheev <kz.zhebrikov@yandex.ru>
2020-10-06 15:14:14 +0300

    correction cors

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,11 @@ async fn main() -> std::io::Result<()> {
             .wrap(
                 Cors::new() // <- Construct CORS middleware builder
                 .supports_credentials() 
-              .allowed_origin_fn(|req| {
-                  req.headers
-                      .get(http::header::ORIGIN)
-                      .map(http::HeaderValue::as_bytes)
-                      .filter(|b| b.ends_with(b".rust-lang.org"))
-                      .is_some()
-              })
-              .allowed_methods(vec!["GET", "POST"])
-              .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-              .allowed_header(http::header::CONTENT_TYPE)
-              .max_age(3600)
-              .finish()
+                .allowed_methods(vec!["GET", "POST"])
+                .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                .allowed_header(http::header::CONTENT_TYPE)
+                .max_age(3600)
+                .finish()
             )            
             .route("/search", web::post().to(search::get_teacher_by_name))
     })
